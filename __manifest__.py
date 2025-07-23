edba885ae9b562638771d7ac5a6d62b8532b5fca
vishal-lokwani <vishal.lokwani@vidhema.com>
2025-07-23 17:47:23 +0530

    icons

--- __manifest__.py
+++ __manifest__.py
@@ -2,19 +2,22 @@
     'name': 'ICP Currency Module', 
     'summary': 'ICP Currency 18 module',
     'description': '''
-        Testing Purpuses.
+        Connects ICP transactions with odoo currency module.
     ''',
     'version': '18.0.1.0.0',
     'category': 'Productivity',
     'license': 'LGPL-3', 
-    'author': 'Odooistic',
-    'website': 'http://www.odooistic.co.uk',
+    'author': 'Vidhema Technologies',
+    'website': 'https://www.vidhema.com',
     'depends': [
         'base',
     ],
+    'price':'100',
+    'currency': 'USD',
     'data': [
         'data/currency_cron.xml',
     ],
+    'image': ['static/description/icp.gif'],
     'installable': True,
     'application': True,
 
